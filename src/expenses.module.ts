585bd4593c7d32960707ef6ff2ee9b12252bb5e4
nniabb <niabilanishvili17@gmail.com>
2024-02-12 22:47:18 +0400

    first commit

--- src/expenses.module.ts
+++ src/expenses.module.ts
@@ -1,11 +1,1 @@
-import { Module } from '@nestjs/common';
-import { ExpenseController } from './expenses.controller';
-import { ExpenseService } from './expenses.service';
 
-
-@Module({
-    controllers: [ExpenseController],
-    providers: [ExpenseService]
-})
-
-export class ExpensesModule {};
